99121e2fb8687284584aa70e15f469f204be2edf
Honey Aswani <aswanihoney8347@gmail.com>
2026-07-01 04:11:06 +0530

    Add home route

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const jwt = require("jsonwebtoken");
 const { UserModel } = require("./model/UserModel");
 
 const PORT = process.env.PORT || 3002;
-const url = process.env.MONGO_URL;
+
 
 const app = express();
 
@@ -22,49 +22,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-/*
-app.get("/addPositions" , async (req,res) =>{
-    let tempPositions =[
-  {
-    product: "CNC",
-    name: "EVEREADY",
-    qty: 2,
-    avg: 316.27,
-    price: 312.35,
-    net: "+0.58%",
-    day: "-1.24%",
-    isLoss: true,
-  },
-  {
-    product: "CNC",
-    name: "JUBLFOOD",
-    qty: 1,
-    avg: 3124.75,
-    price: 3082.65,
-    net: "+10.04%",
-    day: "-1.35%",
-    isLoss: true,
-  },
-];
-    
-tempPositions.forEach((item) =>{
-    let newPosition = new PositionsModel({
-        product: item.product,    
-        name: item.name,
-        qty: item.qty,
-        avg: item.avg,
-        price: item.price,
-        net: item.day,
-        day: item.day ,
-        isLoss: item.isLoss,
-    });
-
-    newPosition.save();
+app.get("/", (req, res) => {
+  res.send("Trading Platform Backend is Running 🚀");
 });
-res.send("Done with positions!");
-
-});
-*/
 
 app.get('/allHoldings',auth, async(req,res) => {
   let allHoldings = await HoldingsModel.find({});
@@ -251,8 +211,7 @@ app.post("/login", async (req, res) => {
 
 });
 
-app.listen(PORT, ()=>{
-    console.log("App started");
+
     mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -266,4 +225,3 @@ app.listen(PORT, ()=>{
     console.log("MongoDB Connection Error:", err);
   });
     
-});
